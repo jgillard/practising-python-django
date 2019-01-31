114cff96843324104cdd68d977a0f1654ebf20ef
James Gillard <jamesgillard@live.co.uk>
2019-01-31 10:30:00 +0700

    Use local Postgres DB. Add models & migration.

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 ```
+
+# Deployment
+
+Currently deployed on every push to master by Heroku.
+
+Site available here: https://django-categories.herokuapp.com/categories/
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+    )
+
+    parent = models.ForeignKey(
+        'self',
+
+        # prevent deletion of referenced upper-level categories
+        on_delete=models.PROTECT,
+
+        # for top-level categories, allow null for the db
+        null=True,
+
+        # for top-level categories, allow blank for form validation
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    OPTION_TYPE_CHOICES = (
+        ('N', 'number'),
+        ('S', 'string'),
+    )
+
+    title = models.CharField(
+        max_length=30,
+        unique=True,
+    )
+
+    category = models.ForeignKey(
+        'Category',
+
+        # prevent deletion of referenced categories
+        on_delete=models.PROTECT
+    )
+
+    answer_type = models.CharField(
+        max_length=1,
+        choices=OPTION_TYPE_CHOICES,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Option(models.Model):
+    title = models.CharField(
+        max_length=30,
+    )
+
+    question = models.ForeignKey(
+        'Question',
+
+        # deleting a question causes its options to be deleted
+        on_delete=models.CASCADE,
+
+        # only allow options for string questions
+        limit_choices_to={'answer_type': 'S'},
+    )
+
+    def __str__(self):
+        return f'{self.question.title} -> {self.title}'
