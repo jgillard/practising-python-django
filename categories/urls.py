7e45ddde799877f5651bbe59736a30f5fe1ff04c
James Gillard <jamesgillard@live.co.uk>
2019-01-31 17:04:55 +0700

    Add the list view pages

--- categories/urls.py
+++ categories/urls.py
@@ -4,7 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('categories/', views.category_list, name='category_list'),
     path('categories/<int:category_id>/', views.category, name='category'),
+
+    path('questions/', views.question_list, name='question_list'),
     path('questions/<int:question_id>/', views.question, name='question'),
+
+    path('options/', views.option_list, name='option_list'),
     path('options/<int:option_id>/', views.option, name='option'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -5,33 +5,48 @@ from .models import Category, Question, Option
 
 
 def index(request):
-    latest_category_list = Category.objects.all()[:5]
-    context = {'latest_category_list': latest_category_list}
+    context = {
+        'category_list': Category.objects.all(),
+        'question_list': Question.objects.all(),
+        'option_list': Option.objects.all(),
+    }
     return render(request, 'index.html', context)
+
+
+def category_list(request):
+    context = {'category_list': Category.objects.all()}
+    return render(request, 'category_list.html', context)
 
 
 def category(request, category_id):
     try:
-        category = Category.objects.get(id=category_id)
-        context = {'category': category}
+        context = {'category': Category.objects.get(id=category_id)}
     except Category.DoesNotExist:
         raise Http404("Category does not exist")
     return render(request, 'category.html', context)
 
 
+def question_list(request):
+    context = {'question_list': Question.objects.all()}
+    return render(request, 'question_list.html', context)
+
+
 def question(request, question_id):
     try:
-        question = Question.objects.get(id=question_id)
-        context = {'question': question}
+        context = {'question': Question.objects.get(id=question_id)}
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'question.html', context)
 
 
+def option_list(request):
+    context = {'option_list': Option.objects.all()}
+    return render(request, 'option_list.html', context)
+
+
 def option(request, option_id):
     try:
-        option = Option.objects.get(id=option_id)
-        context = {'option': option}
+        context = {'option': Option.objects.get(id=option_id)}
     except Option.DoesNotExist:
         raise Http404("Option does not exist")
     return render(request, 'option.html', context)
